1d2f3617f33e044cd23ed99e9d602c7d8e2dea4d
Wer0000 <60045423+Wer0000@users.noreply.github.com>
2023-08-09 12:51:42 +0300

    Add files via upload

--- src/main/java/ua/lann/protankiserver/reflection/AnnotationHelper.java
+++ src/main/java/ua/lann/protankiserver/reflection/AnnotationHelper.java
@@ -9,34 +9,71 @@ import java.util.Enumeration;
 import java.util.List;
 
 public class AnnotationHelper {
-    public static List<Class<?>> load(Class<? extends Annotation> annotation, String basePackage) throws IOException, ClassNotFoundException {
-        List<Class<?>> annotatedClasses = new ArrayList<>();
-        String basePackagePath = basePackage.replace('.', '/');
 
-        ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
-        Enumeration<URL> resources = classLoader.getResources(basePackagePath);
+  public static List<Class<?>> load(Class<? extends Annotation> annotation, String basePackage) throws IOException, ClassNotFoundException {
+    
+    List<Class<?>> annotatedClasses = new ArrayList<>();
+    if (annotation == null || basePackage == null) {
+      return annotatedClasses; 
+    }
+    
+    String basePackagePath = basePackage.replace('.', '/');
 
-        while(resources.hasMoreElements()) {
-            URL resource = resources.nextElement();
-            File file = new File(resource.getFile());
-            findAnnotatedClasses(annotation, basePackage, file, annotatedClasses);
-        }
-
-        return annotatedClasses;
+    ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
+    if (classLoader == null) {
+      return annotatedClasses;
     }
 
-    private static void findAnnotatedClasses(Class<? extends Annotation> annotation, String packageName, File directory, List<Class<?>> classList) throws ClassNotFoundException {
-        File[] files = directory.listFiles();
-        for (File file : files) {
-            if (file.isDirectory()) {
-                findAnnotatedClasses(annotation, packageName + "." + file.getName(), file, classList);
-            } else if (file.getName().endsWith(".class")) {
-                String className = packageName + '.' + file.getName().substring(0, file.getName().length() - 6);
-                Class<?> clazz = Class.forName(className);
-                if (clazz.isAnnotationPresent(annotation)) {
-                    classList.add(clazz);
-                }
-            }
-        }
+    Enumeration<URL> resources = classLoader.getResources(basePackagePath);
+    if (resources == null) {
+      return annotatedClasses;
     }
+
+    while(resources.hasMoreElements()) {
+
+      URL resource = resources.nextElement();
+      if (resource == null) {
+        continue;
+      }
+
+      File file = new File(resource.getFile());
+      if (file == null) {
+        continue;
+      }
+
+      findAnnotatedClasses(annotation, basePackage, file, annotatedClasses);
+    }
+
+    return annotatedClasses;
+  }
+
+  private static void findAnnotatedClasses(Class<? extends Annotation> annotation, String packageName, File directory, List<Class<?>> classList) throws ClassNotFoundException {
+    
+    if (annotation == null || packageName == null || directory == null) {
+      return;
+    }
+    
+    if (classList == null) {
+      classList = new ArrayList<>(); 
+    }
+
+    File[] files = directory.listFiles();
+    if (files == null) {
+      return;
+    }
+
+    for (File file : files) {
+
+      if (file.isDirectory()) {
+        findAnnotatedClasses(annotation, packageName + "." + file.getName(), file, classList);
+      } else if (file.getName().endsWith(".class")) {
+      
+        String className = packageName + '.' + file.getName().substring(0, file.getName().length() - 6);
+        Class<?> clazz = Class.forName(className);
+        if (clazz != null && clazz.isAnnotationPresent(annotation)) {
+          classList.add(clazz);  
+        }
+      }
+    }
+  }
 }
